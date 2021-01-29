ea6f528f89edb79b14269d48395faf6069f395b3
颜飞 <letteryan@qq.com>
2021-01-29 08:45:21 +0800

    update: 升级到Taro3.0.25

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-if (process.env.TARO_ENV === 'weapp') {
-  module.exports = require('./weapp/index')
-  module.exports.default = module.exports
-}
--- dist/weapp/index.js
+++
@@ -1,3 +0,0 @@
-import CanvasDrawer from './component/taro-plugin-canvas/index';
-export { default as CanvasDrawer } from './component/taro-plugin-canvas/index';
-export default CanvasDrawer;
